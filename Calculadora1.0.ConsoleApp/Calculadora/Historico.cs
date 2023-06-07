b37c4fec315dcd6903cb67dbfe063ca5ca0393e2
Kaue Ramos <ramoskauegriss@gmail.com>
2023-06-07 14:57:29 -0300

    Criação do histórico

--- Calculadora1.0.ConsoleApp/Calculadora/Historico.cs
+++ Calculadora1.0.ConsoleApp/Calculadora/Historico.cs
@@ -2,5 +2,31 @@
 {
     public class Historico
     {
+        private List<string> historico;
+
+        public Historico()
+        {
+            historico = new List<string>();
+        }
+
+        public void AdicionarHistorico(string calculo)
+        {
+            historico.Add(calculo);
+        }
+
+        public void LimparHistorico()
+        {
+            historico.Clear();
+        }
+
+        public void ImprimirHistorico()
+        {
+            Console.WriteLine("================= Histórico de cálculos: =================");
+
+            foreach (string calculo in historico)
+            {
+                Console.WriteLine($"{calculo}");
+            }
+        }
     }
 }
